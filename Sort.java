ece88e1590b57fddfb5f02adc50b59c6d29ab943
Fadlullah "Ifad" Hasan <160502733+Enixma05@users.noreply.github.com>
2024-12-18 22:27:10 +0800

    Sort.java

--- Sort.java
+++ Sort.java
@@ -1,7 +1,7 @@
 public class Sort{
     public void bubbleSort(Queue queue) {
             if (queue.isEmpty()) {
-                System.out.println("Antrean kosong, tidak ada yang bisa diurutkan.");
+                System.out.println("Antrian kosong, tidak ada yang bisa diurutkan.");
                 return;
             }
 
@@ -12,7 +12,6 @@ public class Sort{
 
                 while (current != null && current.next != null) {
                     if (current.barang.compareTo(current.next.barang) > 0) {
-                        // Swap the "barang" and "kurir" fields of the nodes
                         String tempBarang = current.barang;
                         String tempKurir = current.kurir;
 
@@ -27,7 +26,5 @@ public class Sort{
                     current = current.next;
                 }
             } while (swapped);
-
-            System.out.println("Antrean telah diurutkan berdasarkan nama barang.");
     }
 }
